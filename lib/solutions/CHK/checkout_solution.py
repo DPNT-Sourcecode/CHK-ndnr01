65baff36d36daae7f3a67e5776faa56aab155363
sbx_user1051 <sbx_user1051@169.254.9.69>
2022-04-10 06:48:11 +0000

    Sun Apr 10 06:48:11 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -13,6 +13,7 @@ def checkout(skus: str) -> int:
     # Dictionary of frequency of items in inventory
     number_of_items_in_inventory = Counter(skus)
     
+    calculate_final_checkout_value(number_of_items_in_inventory, price_of_items)
         
     # if is_illegal_argument(skus):
     #     return 1
@@ -22,10 +23,15 @@ def checkout(skus: str) -> int:
 #     ''' Check for illegal argument'''
 #     return skus[0].isalpha()
 
+# Assumption: Any out of inventory
 def calculate_final_checkout_value(number_of_items_in_inventory: dict,
                                    price_of_items: dict) -> int:
+    '''
+    Using the price of items and any special offers, this calculates the 
+    checkout value after the shopping
+    @return: integer -> final_checkout_value
+    '''
     final_checkout_value = 0
-    checkout_value = 0
     for item in number_of_items_in_inventory:    
         if item in price_of_items:
             # Special Offer A
@@ -36,7 +42,8 @@ def calculate_final_checkout_value(number_of_items_in_inventory: dict,
             elif item == "B":
                 number_of_items_out_of_offer = number_of_items_in_inventory[item] % 2
                 number_of_items_in_offer = number_of_items_in_inventory[item] // 2
-                final_checkout_value = final_checkout_value(number_of_items_out_of_offer*price_of_items[item]) + (number_of_items_in_offer*45)
+                final_checkout_value = final_checkout_value + (number_of_items_out_of_offer*price_of_items[item]) + (number_of_items_in_offer*45)
             else:
-                
+                final_checkout_value = final_checkout_value + number_of_items_in_inventory[item]*price_of_items[item]
+    return final_checkout_value
 
