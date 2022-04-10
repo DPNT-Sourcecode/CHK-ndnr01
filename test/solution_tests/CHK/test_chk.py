cec835d34be08f2f4ee3ed0c25bfd0df8e0c70a3
sbx_user1051 <sbx_user1051@169.254.9.69>
2022-04-10 06:54:11 +0000

    Sun Apr 10 06:54:11 UTC 2022

--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -1,7 +1,12 @@
 from solutions.CHK import checkout_solution
 
-class TestSum():
+class TestCheckout():
     
-    def test_hello_friend(self):
-        assert checkout_solution.hello("John") == "Hello, John!"
+    def test_checkout_value_when_no_special_offer_and_valid_items(self):
+        assert checkout_solution.checkout("ABCD") == 115
 
+    # def test_checkout_value_when_multiple_special_offers_and_valid_items(self):
+    #     assert checkout_solution.checkout("John") == "Hello, John!"
+
+    # def test_checkout_value_when_invalid_items(self):
+    #     assert checkout_solution.checkout("John") == "Hello, John!"
