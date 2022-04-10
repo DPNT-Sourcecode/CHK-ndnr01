419703bb8cc608730a237e1fbe7c709a20f75013
sbx_user1051 <sbx_user1051@169.254.9.69>
2022-04-10 13:01:52 +0000

    Sun Apr 10 13:01:52 UTC 2022

--- lib/solutions/CHK/calculate_group_discount.py
+++ lib/solutions/CHK/calculate_group_discount.py
@@ -3,4 +3,12 @@ Calculates the group discount
 '''
 
 def calculate_group_discounts(number_of_items_in_inventory: dict, price_of_items: dict) -> int:
+    number_of_S_items = number_of_items_in_inventory.get("S", 0)
+    number_of_S_items = number_of_items_in_inventory.get("S", 0)
+    number_of_S_items = number_of_items_in_inventory.get("S", 0)
+    number_of_S_items = number_of_items_in_inventory.get("S", 0)
+    number_of_S_items = number_of_items_in_inventory.get("S", 0)
     
+    
+def _total_number_items_applicable_for_group_discounts():
+    list_of_items_for_group_discount = ["S"]
