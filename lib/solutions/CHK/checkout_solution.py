39d3359ff9679a26a257af8c65cab613a5f3eecd
sbx_user1051 <sbx_user1051@169.254.9.69>
2022-04-10 06:18:11 +0000

    Sun Apr 10 06:18:11 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -8,6 +8,7 @@ def checkout(skus):
     @return: Total checkout value
     '''
     dict_of_items = {"A": 50, "B": 30, "C": 20, "D": 15}
+    
     for item in skus:
         if item not in dict_of_items:
             return -1
@@ -19,5 +20,6 @@ def checkout(skus):
 # def is_illegal_argument(skus):
 #     ''' Check for illegal argument'''
 #     return skus[0].isalpha()
+
 
 
