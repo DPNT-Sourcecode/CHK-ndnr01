5aeb4842453c0308d8d18a1366ece0caf086855d
sbx_user1051 <sbx_user1051@169.254.9.69>
2022-04-10 08:20:22 +0000

    Sun Apr 10 08:20:22 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -68,6 +68,7 @@ def calculate_special_offer_for_B(item: str, number_of_items_in_inventory: dict,
     '''
     number_of_items_out_of_offer = number_of_items_in_inventory[item] % 2
     number_of_items_in_offer = number_of_items_in_inventory[item] // 2
+    
     return (number_of_items_out_of_offer*price_of_items[item]) + (number_of_items_in_offer*45)
 
 def calculate_special_offer_for_E(item: str, number_of_items_in_inventory: dict, price_of_items: dict) -> int:
@@ -85,4 +86,5 @@ def calculate_special_offer_for_E(item: str, number_of_items_in_inventory: dict,
 1. Rearchitecture the code into separate files etc
 2. Extract the common functionality i.e. // and % into separate function as repeated
 '''
+
 
