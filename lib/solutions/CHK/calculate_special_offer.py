9b46bd5767adc97bc76608d15eb94b0ff7ffe73f
sbx_user1051 <sbx_user1051@169.254.9.69>
2022-04-10 11:00:22 +0000

    Sun Apr 10 11:00:22 UTC 2022

--- lib/solutions/CHK/calculate_special_offer.py
+++ lib/solutions/CHK/calculate_special_offer.py
@@ -46,12 +46,12 @@ def calculate_special_offer_for_K(number_of_item: int, price_of_item: int) -> in
                             value_of_offer=150)
 
 #================================================================#
-def calculate_special_offer_for_E(item: str, number_of_items_in_inventory: dict, price_of_items: dict) -> int:
-    '''
-    This assumes that if B alread in inventory, then it will not be charged. If B not in inventory, it
-    will be given as free rather than price of B being reduced from the final checkout value
-    '''
-    return (number_of_items_in_inventory[item]*price_of_items[item])
+# def calculate_special_offer_for_E(item: str, number_of_items_in_inventory: dict, price_of_items: dict) -> int:
+#     '''
+#     This assumes that if B alread in inventory, then it will not be charged. If B not in inventory, it
+#     will be given as free rather than price of B being reduced from the final checkout value
+#     '''
+#     return (number_of_items_in_inventory[item]*price_of_items[item])
 
 #========================HELPER FUNCTIONS FOR OFFER CALCULATION========================================#
 def _calculate_single_offer(number_of_item: int, price_of_item: int, number_of_items_for_offer: int, value_of_offer: int):
@@ -59,10 +59,11 @@ def _calculate_single_offer(number_of_item: int, price_of_item: int, number_of_i
     number_of_items_in_offer = number_of_item // number_of_items_for_offer
     return (number_of_items_out_of_offer*price_of_item) + (number_of_items_in_offer*value_of_offer)
     
-# def _calculate_combined_offer():
-#     number_of_items_in_offer_of_ten = number_of_items_in_inventory[item] // 10
-#     number_of_items_not_in_offer_of_ten = number_of_items_in_inventory[item] % 10
-#     number_of_items_in_offer_of_five = number_of_items_not_in_offer_of_ten // 5
-#     number_of_items_out_of_offer = number_of_items_not_in_offer_of_ten % 5
-#     return (number_of_items_in_offer_of_ten*80) + (number_of_items_in_offer_of_five*45) + (number_of_items_out_of_offer*price_of_items[item])
+def _calculate_combined_offer(number_of_item: int, price_of_item: int, number_of_items_for_offer: int, value_of_offer: int):
+    number_of_items_in_offer_of_ten = number_of_items_in_inventory[item] // 10
+    number_of_items_not_in_offer_of_ten = number_of_items_in_inventory[item] % 10
+    number_of_items_in_offer_of_five = number_of_items_not_in_offer_of_ten // 5
+    number_of_items_out_of_offer = number_of_items_not_in_offer_of_ten % 5
+    return (number_of_items_in_offer_of_ten*80) + (number_of_items_in_offer_of_five*45) + (number_of_items_out_of_offer*price_of_items[item])
 #========================================================================================================#
+
