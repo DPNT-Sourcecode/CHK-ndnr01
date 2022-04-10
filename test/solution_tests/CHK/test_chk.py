bd016ef73ee9338a86c37023db6e33202cf3989f
sbx_user1051 <sbx_user1051@169.254.9.69>
2022-04-10 08:11:22 +0000

    Sun Apr 10 08:11:22 UTC 2022

--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -10,7 +10,9 @@ class TestCheckout():
 
     def test_checkout_value_when_invalid_items(self):
         assert checkout_solution.checkout("ABCD1234") == -1
-        
+    
+    
     def test_checkout_value_when_multiple_offers_for_A(self):
-        assert checkout_solution.checkout("AAAAAAAAAAAAAA") == -1
+        assert checkout_solution.checkout("AAAAAAAAAAAAAA") == 580
+        
 
