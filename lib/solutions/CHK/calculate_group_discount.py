e26b6529611abaf6ef9f7c081ae4bc89b483a1fd
sbx_user1051 <sbx_user1051@169.254.9.69>
2022-04-10 13:22:52 +0000

    Sun Apr 10 13:22:52 UTC 2022

--- lib/solutions/CHK/calculate_group_discount.py
+++ lib/solutions/CHK/calculate_group_discount.py
@@ -11,11 +11,14 @@ def calculate_group_discounts(number_of_items_in_inventory: dict, price_of_items
     number_of_items_left_from_group_discounts =  total_number_of_items_in_group_discount % 3
     
     final_price_of_group_discounts = number_of_group_discounts * 45
-    if number_of_items_left_from_group_discounts == 1:
-        return (min(list_of_prices_of_group_discount_items)+final_price_of_group_discounts)
     
-    list_of_prices_of_group_discount_items = sorted(list_of_prices_of_group_discount_items)
-    return (list_of_prices_of_group_discount_items[0]+list_of_prices_of_group_discount_items[1]+final_price_of_group_discounts)
+    if number_of_items_left_from_group_discounts == 0:
+        return final_price_of_group_discounts
+    elif number_of_items_left_from_group_discounts == 1:
+        return (min(list_of_prices_of_group_discount_items)+final_price_of_group_discounts)
+    else:
+        list_of_prices_of_group_discount_items = sorted(list_of_prices_of_group_discount_items)
+        return (list_of_prices_of_group_discount_items[0]+list_of_prices_of_group_discount_items[1]+final_price_of_group_discounts)
     
 def _total_number_items_and_prices_applicable_for_group_discounts(number_of_items_in_inventory:dict, price_of_items: dict):
     list_of_items_for_group_discount = ["S","T","X","Y","Z"]
