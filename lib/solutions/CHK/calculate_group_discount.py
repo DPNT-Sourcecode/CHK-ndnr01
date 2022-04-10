2b71accac8665d54757d83b1fa8dad73089be493
sbx_user1051 <sbx_user1051@169.254.9.69>
2022-04-10 13:07:52 +0000

    Sun Apr 10 13:07:52 UTC 2022

--- lib/solutions/CHK/calculate_group_discount.py
+++ lib/solutions/CHK/calculate_group_discount.py
@@ -15,5 +15,7 @@ def _total_number_items_applicable_for_group_discounts(number_of_items_in_invent
     list_of_prices_of_group_discount_items = []
     total_number_of_items_in_group_discount = 0
     for item in list_of_items_for_group_discount:
-        total_number_of_items_in_group_discount +=total_number_of_items_in_group_discount+number_of_items_in_inventory.get(item, 0)
-        list_of_prices_of_group_discount_items.append()
+        if item in number_of_items_in_inventory:
+            total_number_of_items_in_group_discount +=total_number_of_items_in_group_discount+number_of_items_in_inventory[item]
+            list_of_prices_of_group_discount_items.append(price_of_items[item])
+    return (total_number_of_items_in_group_discount, list_of_prices_of_group_discount_items)
