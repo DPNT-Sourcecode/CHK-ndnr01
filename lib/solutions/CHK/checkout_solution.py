fdb7664894e5ae42586c518d4fadea44dede5505
sbx_user1051 <sbx_user1051@169.254.9.69>
2022-04-10 08:08:22 +0000

    Sun Apr 10 08:08:22 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -60,7 +60,7 @@ def calculate_special_offer_for_A(item: str, number_of_items_in_inventory: dict,
     number_of_items_not_in_offer_of_five = number_of_items_in_inventory[item] % 5
     number_of_items_in_offer_of_three = number_of_items_not_in_offer_of_five // 3
     number_of_items_out_of_offer = number_of_items_not_in_offer_of_five % 3
-    return (number_of_items_in_offer_of_five*200) + (number_of_items_out_of_offer*price_of_items[item]) + (number_of_items_in_offer*130)
+    return (number_of_items_in_offer_of_five*200) + (number_of_items_in_offer_of_three*130) + (number_of_items_out_of_offer*price_of_items[item])
 
 def calculate_special_offer_for_B(item: str, number_of_items_in_inventory: dict, price_of_items: dict) -> int:
     '''
@@ -79,3 +79,4 @@ def calculate_special_offer_for_E(item: str, number_of_items_in_inventory: dict,
     number_of_items_out_of_offer = number_of_items_in_inventory[item] % 2
     number_of_items_in_offer = number_of_items_in_inventory[item] // 2
     return (number_of_items_out_of_offer*price_of_items[item]) + (number_of_items_in_offer*45)
+
--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -10,3 +10,7 @@ class TestCheckout():
 
     def test_checkout_value_when_invalid_items(self):
         assert checkout_solution.checkout("ABCD1234") == -1
+        
+    def test_checkout_value_when_multiple_offers_for_A(self):
+        assert checkout_solution.checkout("AAAAAAAAAAAAAA") == -1
+
